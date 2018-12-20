dfc2d41ec06216ad47723b7e6bf2fc9d0d647400
Ilya Subota <ilyasubota777@gmail.com>
2018-12-20 20:11:06 +0300

    Practicum 2

--- practicum2/calendar.js
+++ practicum2/calendar.js
@@ -1,10 +1,11 @@
 function Calendar(id, year, month) {
 var Dlast = new Date(year,month+1,0).getDate(),
     D = new Date(year,month,Dlast),
-    DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
+    DNlast = D.getDay(),
     DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
     calendar = '<tr>',
-    month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+    m = document.querySelector('#'+id+' option[value="' + D.getMonth() + '"]'),
+    g = document.querySelector('#'+id+' input');
 if (DNfirst != 0) {
   for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
 }else{
@@ -14,7 +15,24 @@ for(var  i = 1; i <= Dlast; i++) {
   if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
     calendar += '<td class="today">' + i;
   }else{
-    calendar += '<td>' + i;
+    if (  // список официальных праздников
+        (i == 1 && D.getMonth() == 0 && ((D.getFullYear() > 1897 && D.getFullYear() < 1930) || D.getFullYear() > 1947)) || // Новый год
+        (i == 2 && D.getMonth() == 0 && D.getFullYear() > 1992) || // Новый год
+        ((i == 3 || i == 4 || i == 5 || i == 6 || i == 8) && D.getMonth() == 0 && D.getFullYear() > 2004) || // Новый год
+        (i == 7 && D.getMonth() == 0 && D.getFullYear() > 1990) || // Рождество Христово
+        (i == 23 && D.getMonth() == 1 && D.getFullYear() > 2001) || // День защитника Отечества
+        (i == 8 && D.getMonth() == 2 && D.getFullYear() > 1965) || // Международный женский день
+        (i == 1 && D.getMonth() == 4 && D.getFullYear() > 1917) || // Праздник Весны и Труда
+        (i == 9 && D.getMonth() == 4 && D.getFullYear() > 1964) || // День Победы
+        (i == 12 && D.getMonth() == 5 && D.getFullYear() > 1990) || // День России (декларации о государственном суверенитете Российской Федерации ознаменовала окончательный Распад СССР)
+        (i == 7 && D.getMonth() == 10 && (D.getFullYear() > 1926 && D.getFullYear() < 2005)) || // Октябрьская революция 1917 года
+        (i == 8 && D.getMonth() == 10 && (D.getFullYear() > 1926 && D.getFullYear() < 1992)) || // Октябрьская революция 1917 года
+        (i == 4 && D.getMonth() == 10 && D.getFullYear() > 2004) // День народного единства, который заменил Октябрьскую революцию 1917 года
+       ) {
+      calendar += '<td class="holiday">' + i;
+    }else{
+      calendar += '<td>' + i;
+    }
   }
   if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
     calendar += '<tr>';
@@ -22,19 +40,14 @@ for(var  i = 1; i <= Dlast; i++) {
 }
 for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
 document.querySelector('#'+id+' tbody').innerHTML = calendar;
-document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
-document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
-document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
-if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+g.value = D.getFullYear();
+m.selected = true;
+if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {
     document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
 }
+document.querySelector('#'+id+' option[value="' + new Date().getMonth() + '"]').style.color = 'rgb(220, 0, 0)'; // в выпадающем списке выделен текущий месяц
 }
-Calendar("calendar", new Date().getFullYear(), new Date().getMonth());
-// переключатель минус месяц
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)-1);
-}
-// переключатель плюс месяц
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)+1);
+Calendar("calendar",new Date().getFullYear(),new Date().getMonth());
+document.querySelector('#calendar').onchange = function Calendar() {
+  Calendar("calendar",document.querySelector('#calendar input').value,parseFloat(document.querySelector('#calendar select').options[document.querySelector('#calendar select').selectedIndex].value));
 }
